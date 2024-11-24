5d9d8d455acb55ae3d629c40fad7f2cd56312284
Daniel <danieldcn05@gmail.com>
2024-11-24 21:04:59 +0100

    # VERSION FINAL

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class MainApp(tk.Tk):
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("View4")
+        self.show_frame("View1")
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
